abf6fa35bb35febc12889974e1c8daedf2435c95
Jose Chavez <jose.e.chavez@gmail.com>
2021-05-26 22:55:14 -0500

    Renamed to tradernext-ng

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('tradernext-ui app is running!');
+    expect(await page.getTitleText()).toEqual('tradernext-ng app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tradernext-ui'`, () => {
+  it(`should have as title 'tradernext-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tradernext-ui');
+    expect(app.title).toEqual('tradernext-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tradernext-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tradernext-ng app is running!');
   });
 });
